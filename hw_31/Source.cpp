d8d1d4078128cf7d7df2491b078a1ff0b3668941
Belo_cg86@itstepru.onmicrosoft.com <Belo_cg86@itstepru.onmicrosoft.com>
2022-04-20 22:35:15 +0500

    2 задача

--- hw_31/Source.cpp
+++ hw_31/Source.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<time.h>
+#include<stdlib.h>
 using namespace std;
 int randNum(int n, int m) {
 	if(n>m)
@@ -11,6 +12,17 @@ int randNum(int n, int m) {
 	t = rand() % (m-n+1) + n;
 	cout << "Случайное число в заданном диапазоне: ";
 	return t;
+}
+int foo(string &str) {
+	size_t begin = str.find_first_of("0123456789");
+	size_t end = str.find_last_of("0123456789");
+	string num = str.substr(begin, end - begin + 1);
+	int str_size = str.find(" ");
+	int num2 = stoi(num);
+	if (num2==str_size)
+	return true;
+	return false;
+	
 }
 int main() {
 	setlocale(LC_ALL, "ru");
@@ -28,6 +40,31 @@ int main() {
 	catch (const invalid_argument &ex) {
 	cout << "Ошибка "<<ex.what() << endl;
 	}
+
+
+	//---------------------------------------- -
+	cin.ignore();
+	cout << "\nЗадача 2\n";
+	string str;
+	cout << "Введите строку и ее длину через пробел:\n";
+	getline(cin, str);
+	try {
+		foo(str) == true ? cout << "TRUE" : cout << "FALSE";
+	}
+	catch (const exception& ex) {
+		cout << "ошибка " << ex.what() << endl;
+	}
+
+		
+	
+	
+	
+
+	
+
+
+
+
 
 	
 	system("pause>null");
